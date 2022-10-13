fd1b1a76458ff873de54f9f84a85d4911ef66bd6
Vladyslav <vkadyrov@protonmail.com>
2022-10-13 18:41:10 -0400

    commit tree 18636b25492e03c39a147c92dec31ffef6af383d

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let i = 0; i < word.length / 2; i++) {
+    const j = word.length - 1 - i
+    if (word[i] !== word[j]) return false;
+  }
+  return true;
 }
 
 /* 
-  Add your pseudocode here
+  iterate from beginning to the middle
+    check each letter to the corresponding letter from the end
+      if any letter don't match, return false
+  
+  else, return true
 */
 
 /*
-  Add written explanation of your solution here
+  Function returns true is the word is palidrome. Checks first and last, second and next to last letters.
 */
 
 // You can run `node index.js` to view these console logs
